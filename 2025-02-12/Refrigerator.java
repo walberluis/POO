abeb33c5f62719bd172cc0647401d2fdd5d43c70
Walber Luis Santos da Paixão <144743573+walberluis@users.noreply.github.com>
2025-02-18 00:01:40 -0300

    Update Refrigerator.java

--- 2025-02-12/Refrigerator.java
+++ 2025-02-12/Refrigerator.java
@@ -4,7 +4,7 @@ public class Refrigerator extends Product {
 
     private int size;
 
-    public Refrigerator(String brand, String price, int size) {
+    public Refrigerator(String brand, double price, int size) {
         super(brand, price);
         this.size = size;
 
