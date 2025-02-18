1e7f652af91ce2c486b9370683b3ac634de86859
Walber Luis Santos da Paixão <144743573+walberluis@users.noreply.github.com>
2025-02-17 22:58:57 -0300

    Create Product.java

---
+++ 2025-02-12/Product.java
@@ -0,0 +1,17 @@
+class Product {
+    private String brand;
+    private double price;
+
+    public Product(String brand, double price) {
+        this.brand = brand;
+        this.price = price;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
