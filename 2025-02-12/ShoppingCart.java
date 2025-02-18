5a0aaabcb6445b97e4e82224b98aa9b9fd5073a4
Walber Luis Santos da Paixão <144743573+walberluis@users.noreply.github.com>
2025-02-17 23:51:51 -0300

    Create ShoppingCart.java

---
+++ 2025-02-12/ShoppingCart.java
@@ -0,0 +1,48 @@
+package org.example;
+import java.util.ArrayList;
+
+
+public class ShoppingCart {
+
+    private int customerId;
+    private ArrayList<Product> productList;
+
+    public ShoppingCart(int customerId, ArrayList<Product> ProductList) {
+        this.customerId = customerId;
+        this.productList = ProductList;
+    }
+
+    public void addProduct(Product product) {
+        productList.add(product);
+    }
+
+    public void removeProduct(Product product) {
+        productList.remove(product);
+    }
+
+    public String getContents() {
+        StringBuilder contents = new StringBuilder("");
+
+        for (Product p : productList) {
+            contents.append(p.getBrand()).append(" - $").append(p.getPrice()).append("\n");
+        }
+        return contents.toString();
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public int getItemCount() {
+        return productList.size();
+    }
+
+    public double getTotalPrice() {
+        double total = 0;
+        for (Product product : productList) {
+            total += product.getPrice();
+        }
+        return total;
+    }
+
+}
