77f312fcbe87dffd457e4e876ec6fe2f571c4d7d
Walber Luis Santos da Paixão <144743573+walberluis@users.noreply.github.com>
2025-02-17 22:59:46 -0300

    Create Refrigerator.java

---
+++ 2025-02-12/Refrigerator.java
@@ -0,0 +1,12 @@
+class Refrigerator extends Product {
+    private int size;
+
+    public Refrigerator(String brand, double price, int size) {
+        super(brand, price);
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
